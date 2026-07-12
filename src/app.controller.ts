4dbc0db8ce2494d904c4ffda80c65b80c56b35a0
Gonzalo <gonzasoler.20@gmail.com>
2026-07-12 15:33:41 -0300

    Revert "test: endpoint publico GET /health (deploy-test)"
    This reverts commit 08e3148082b481486e5f335f37a863328167f4c8.

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('health')
-export class AppController {
-  @Get()
-  getHealth() {
-    return { status: 'ok', version: 'deploy-test', date: '2026-07-12' };
-  }
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { InfraestructureModule } from './infraestructure/infraestructure.module';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), InfraestructureModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
